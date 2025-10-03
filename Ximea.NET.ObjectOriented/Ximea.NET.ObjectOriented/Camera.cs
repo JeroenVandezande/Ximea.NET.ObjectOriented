c77e7ef2f1750476cd18fa24ae006cb8bac76d38
Jeroen Vandezande <Jeroen.Vandezande@spathi.be>
2025-10-03 10:08:56 +0200

    Adds camera class implementation
    Adds camera class, implementing the ICamera interface.
    
    This class manages camera properties, including width, height, offsets, and exposure, by interacting with the Ximea camera API.
    
    Provides properties to get min/max ranges for width, height, x/y offsets from the camera.
    
    Also includes basic event handling for image data.

--- Ximea.NET.ObjectOriented/Ximea.NET.ObjectOriented/Camera.cs
+++ Ximea.NET.ObjectOriented/Ximea.NET.ObjectOriented/Camera.cs
@@ -1,5 +1,6 @@
 ﻿using CameraInterface;
 using UnitsNet;
+using xiApi.NET;
 
 namespace Ximea.NET.ObjectOriented;
 
@@ -14,11 +15,59 @@ public class Camera: ICamera
     public int Height { get; set; }
     public int XOffset { get; set; }
     public int YOffset { get; set; }
-    public (int Min, int Max) WidthRange { get; }
-    public (int Min, int Max) HeightRange { get; }
-    public (int Min, int Max) XOffsetRange { get; }
-    public (int Min, int Max) YOffsetRange { get; }
-    public Duration Exposure { get; set; }
+    public (int Min, int Max) WidthRange {
+        get
+        {
+            _camera.GetParam(PRM.WIDTH_MIN, out int min);
+            _camera.GetParam(PRM.WIDTH_MAX, out int max);
+            return (min, max);
+        }
+    }
+
+    public (int Min, int Max) HeightRange
+    {
+        get
+        {
+            _camera.GetParam(PRM.HEIGHT_MIN, out int min);
+            _camera.GetParam(PRM.HEIGHT_MAX, out int max);
+            return (min, max);
+        }
+    }
+
+    public (int Min, int Max) XOffsetRange
+    {
+        get
+        {
+            _camera.GetParam(PRM.OFFSET_X_MIN, out int min);
+            _camera.GetParam(PRM.OFFSET_X_MAX, out int max);
+            return (min, max);
+        }
+    }
+
+    public (int Min, int Max) YOffsetRange
+    {
+        get
+        {
+            _camera.GetParam(PRM.OFFSET_Y_MIN, out int min);
+            _camera.GetParam(PRM.OFFSET_Y_MAX, out int max);
+            return (min, max);
+        }
+    }
+    public Duration Exposure 
+    {
+        get
+        {
+            _camera.GetParam(PRM.EXPOSURE, out int exposure);
+            return Duration.FromMicroseconds(exposure);
+        } 
+        set
+        {
+            _camera.SetParam(PRM.EXPOSURE, (int)value.Microseconds);
+        } 
+    }
+    
     public ImageFormat CameraOutputFormat { get; set; }
     public event EventHandler<ImageData>? ImageReceived;
+    
+    private readonly xiCam _camera = new();
 }
