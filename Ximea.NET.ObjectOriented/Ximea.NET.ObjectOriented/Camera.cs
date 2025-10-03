f0887d8612058d8c9c94618c8df8707816f261c4
Jeroen Vandezande <Jeroen.Vandezande@spathi.be>
2025-10-03 09:48:50 +0200

    Adds initial project structure and files
    Sets up the basic project structure for the Ximea.NET.ObjectOriented project.
    
    This includes:
    - Creating the .gitignore file to exclude obj/ and bin/Debug directories
    - Adding Rider IDE configuration files
    - Creating the solution file (.sln)
    - Implementing a basic Camera class that implements the ICamera interface
    - Creating the project file (.csproj) and adding dependencies to CameraInterface and xiAPI.NET.CrossPlatform

---
+++ Ximea.NET.ObjectOriented/Ximea.NET.ObjectOriented/Camera.cs
@@ -0,0 +1,24 @@
+﻿using CameraInterface;
+using UnitsNet;
+
+namespace Ximea.NET.ObjectOriented;
+
+public class Camera: ICamera
+{
+    public void FireManualTrigger()
+    {
+       
+    }
+
+    public int Width { get; set; }
+    public int Height { get; set; }
+    public int XOffset { get; set; }
+    public int YOffset { get; set; }
+    public (int Min, int Max) WidthRange { get; }
+    public (int Min, int Max) HeightRange { get; }
+    public (int Min, int Max) XOffsetRange { get; }
+    public (int Min, int Max) YOffsetRange { get; }
+    public Duration Exposure { get; set; }
+    public ImageFormat CameraOutputFormat { get; set; }
+    public event EventHandler<ImageData>? ImageReceived;
+}
